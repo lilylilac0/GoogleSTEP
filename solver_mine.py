ecf611f4c0df76772c968ddfa9ea4eb95e20e5ed
AiHamada <128439499+lilylilac0@users.noreply.github.com>
2024-06-18 15:11:37 +0900

    Update solver_mine.py

--- solver_mine.py
+++ solver_mine.py
@@ -7,32 +7,6 @@ from heapq import heappush, heappop
 def distance(city1, city2):
     return math.sqrt((city1[0] - city2[0]) ** 2 + (city1[1] - city2[1]) ** 2)
 
-
-# ①初期経路を作成
-#   2近似解法を採用(プリム法)
-#       →c近似解法とは、最悪でもc倍の近似解が得られるアルゴリズムのこと
-#   手順）   1, 最小全域木Tを求める
-#               ここで以下のプリム法を使う
-#               1, 都市を頂点とし、辺を一本も含まないグラフTを用意する
-#               2, スタート点を一つ決める
-#               3, 追加できる辺がなくなるまで以下を繰り返す
-#                   ・スタート点から辿れる辺の中で最も重みが小さい辺をTに追加する
-#                    注意：つながっているすべての点から辿れる辺かつ追加しても閉路にならないような辺であることが条件
-#               4, 最終的に出来上がったTが最小全域木である
-#           2, Tの枝を複製したグラフを作り、一筆書きで一周する
-#               オイラー路（何回でも同じ辺、頂点を通って良い）を作る
-#           3, すでに訪れた都市は再度訪れないようにし、全都市をちょうど一回ずつ通る閉路を作る
-#               Hierholzer's Algorithm
-#               →・ある点から始めてまたその点に戻るパスを見つけて（閉路）、通ったパスを除去
-#                ・上記を繰り返してできた複数の閉路を繋ぎ合わせる
-# ②経路を最適化
-#   2-opt法を採用
-#
-# その他の解法：
-#   プリム法 - クラスカル法
-#   Hierholzer's Algorithm - Fleury's Algorithm
-
-
 # プリム法で最小全域木を求める
 def prim(cities):
     edge = []               #最小全域木に必要なへ辺(辺の重み、端点１、端点２)のリスト
@@ -137,9 +111,9 @@ def solve():
     edges = prim(cities)
     eularian_path = create_eulerian_path(edges)
     hamiltonian_path = create_hamiltonian_path(eularian_path)
-    tour = two_opt(hamiltonian_path, cities)
 
-    # ②　経路を最適化
+    # ②　経路を最適化    
+    tour = two_opt(hamiltonian_path, cities)
 
     # 経路の総コストを計算
     total = calculate_total_distance(tour, cities)
